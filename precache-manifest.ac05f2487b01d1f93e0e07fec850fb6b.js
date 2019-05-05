96e70c816fef9a52b488e616ab643ab8f716567c
Michal <michal.pyszynski93@gmail.com>
2019-05-05 22:06:19 +0200

    Updates

---
+++ precache-manifest.ac05f2487b01d1f93e0e07fec850fb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bee41d3d3dc5a1c627518a837d13a9",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "7300a83981eb0d33ab84",
+    "url": "/Calculator/static/css/main.e4bd83e5.chunk.css"
+  },
+  {
+    "revision": "e7b69c06955e425d79ce",
+    "url": "/Calculator/static/js/2.5212c4f3.chunk.js"
+  },
+  {
+    "revision": "7300a83981eb0d33ab84",
+    "url": "/Calculator/static/js/main.547acfe8.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  },
+  {
+    "revision": "feb45f14a374652daccf0df5851b58d1",
+    "url": "/Calculator/static/media/food.feb45f14.jpg"
+  }
+]);
